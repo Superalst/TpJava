f9d5c4f23e3c0719b9f80ff1bf9eab6ff920b1f8
Agus <Agus@DESKTOP-B9JAJ22.fibertel.com.ar>
2021-10-13 20:27:34 -0300

    Modificacion persona, Creacion de la carpeta logic y la clase main.

--- TpJava2021/TpChangas/src/entidades/Persona.java
+++ TpJava2021/TpChangas/src/entidades/Persona.java
@@ -1,6 +1,5 @@
 package entidades;
 
-import java.util.Date;
 
 public class Persona {
 
@@ -11,6 +10,55 @@ public class Persona {
 	private String oficio;
 	private String areaTrabajo;
 	private Double valuacionPromedio;
+	public int getDni() {
+		return dni;
+	}
+	public void setDni(int dni) {
+		this.dni = dni;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	public String getOficio() {
+		return oficio;
+	}
+	public void setOficio(String oficio) {
+		this.oficio = oficio;
+	}
+	public String getAreaTrabajo() {
+		return areaTrabajo;
+	}
+	public void setAreaTrabajo(String areaTrabajo) {
+		this.areaTrabajo = areaTrabajo;
+	}
+	public Double getValuacionPromedio() {
+		return valuacionPromedio;
+	}
+	public void setValuacionPromedio(Double valuacionPromedio) {
+		this.valuacionPromedio = valuacionPromedio;
+	}
+	
+	@Override
+	public String toString() {
+		return "Persona [dni=" + dni + ", nombre=" + nombre + ", apellido=" + apellido + ", telefono=" + telefono
+				+ ", oficio=" + oficio + ", areaTrabajo=" + areaTrabajo + ", valuacionPromedio=" + valuacionPromedio
+				+ "]";
+	}
 
 	
 	
