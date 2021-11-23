ebd90a99234711144e40dc38ef8f01d9b896e90f
SantiC96 <santiago.cancio96@gmail.com>
2021-11-23 17:08:14 -0300

    Push Index

--- TpJava2021/TpChangas/src/entidades/CategoriaTrabajo.java
+++ TpJava2021/TpChangas/src/entidades/CategoriaTrabajo.java
@@ -1,9 +1,13 @@
 package entidades;
 
+import java.util.LinkedList;
+
 public class CategoriaTrabajo {
 
 	private int idCategoria;
 	private String descripcion;
+	private LinkedList<Persona> personas;
+	
 	public int getIdCategoria() {
 		return idCategoria;
 	}
@@ -16,7 +20,10 @@ public class CategoriaTrabajo {
 	public void setDescripcion(String descripcion) {
 		this.descripcion = descripcion;
 	}
-	
-	
-
+	public LinkedList<Persona> getPersonas() {
+		return personas;
+	}
+	public void setPersonas(LinkedList<Persona> personas) {
+		this.personas = personas;
+	}
 }
--- TpJava2021/TpChangas/src/entidades/Persona.java
+++ TpJava2021/TpChangas/src/entidades/Persona.java
@@ -1,5 +1,6 @@
 package entidades;
 
+import java.util.LinkedList;
 
 public class Persona {
 
@@ -10,6 +11,11 @@ public class Persona {
 	private String oficio;
 	private String areaTrabajo;
 	private Double valuacionPromedio;
+	private LinkedList<CategoriaTrabajo> categoriasTrabajo;
+	private Direccion direccion;
+	private LinkedList<Localidad> localidades;
+	private LinkedList<Trabajo> trabajos;
+	
 	public int getDni() {
 		return dni;
 	}
@@ -52,16 +58,34 @@ public class Persona {
 	public void setValuacionPromedio(Double valuacionPromedio) {
 		this.valuacionPromedio = valuacionPromedio;
 	}
-	
-	@Override
-	public String toString() {
-		return "\nPersona [dni=" + dni + ", nombre=" + nombre + ", apellido=" + apellido + ", telefono=" + telefono
-				+ ", oficio=" + oficio + ", areaTrabajo=" + areaTrabajo + ", valuacionPromedio=" + valuacionPromedio
-				+ "]";
+	public LinkedList<CategoriaTrabajo> getCategoriasTrabajo() {
+		return categoriasTrabajo;
+	}
+	public void setCategoriasTrabajo(LinkedList<CategoriaTrabajo> categoriasTrabajo) {
+		this.categoriasTrabajo = categoriasTrabajo;
+	}
+	public Direccion getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(Direccion direccion) {
+		this.direccion = direccion;
+	}
+	public LinkedList<Localidad> getLocalidades() {
+		return localidades;
+	}
+	public void setLocalidades(LinkedList<Localidad> localidades) {
+		this.localidades = localidades;
+	}
+	public LinkedList<Trabajo> getTrabajos() {
+		return trabajos;
+	}
+	public void setTrabajos(LinkedList<Trabajo> trabajos) {
+		this.trabajos = trabajos;
 	}
 	
 	public Persona(int dni, String nombre, String apellido, String telefono, String oficio, String areaTrabajo,
-			Double valuacionPromedio) {
+			Double valuacionPromedio, LinkedList<CategoriaTrabajo> categoriasTrabajo, Direccion direccion,
+			LinkedList<Localidad> localidades, LinkedList<Trabajo> trabajos) {
 		super();
 		this.dni = dni;
 		this.nombre = nombre;
@@ -70,11 +94,21 @@ public class Persona {
 		this.oficio = oficio;
 		this.areaTrabajo = areaTrabajo;
 		this.valuacionPromedio = valuacionPromedio;
+		this.categoriasTrabajo = categoriasTrabajo;
+		this.direccion = direccion;
+		this.localidades = localidades;
+		this.trabajos = trabajos;
 	}
+	
 	public Persona() {
 		super();
 	}
-
 	
-	
+	@Override
+	public String toString() {
+		return "Persona [dni=" + dni + ", nombre=" + nombre + ", apellido=" + apellido + ", telefono=" + telefono
+				+ ", oficio=" + oficio + ", areaTrabajo=" + areaTrabajo + ", valuacionPromedio=" + valuacionPromedio
+				+ ", categoriasTrabajo=" + categoriasTrabajo + ", direccion=" + direccion + ", localidades="
+				+ localidades + ", trabajos=" + trabajos + "]";
+	}
 }
